8f8f52084798f29939f35e099f05d70299676ea3
dtpthao <htluc@selab.hcmus.edu.vn>
2023-05-29 03:50:35 +0000

    Nothing special

--- slurm_run.sh
+++ slurm_run.sh
@@ -12,30 +12,41 @@ cd /home/dtpthao/workspace/yolov5
 
 # Train
 # python train.py \
-# --img 480 --batch 64 --epochs 300 \
+# --img 480 --batch 64 --epochs 1 \
 # --data /home/dtpthao/workspace/yolov5/aim_folds_bigger_box/fold_0/annotations/aim_fold_0.yaml \
 # --weights yolov5s.pt \
 # --name 'yolov5s_fold_0_bigger' \
 # --patience 300
 
 # Validation
-# fold=4
+# fold=0
 # python val.py \
 # --img 480 --batch 1 \
-# --data /home/dtpthao/workspace/yolov5/aim_folds/fold_$fold/annotations/aim_fold_$fold.yaml \
+# --data /home/dtpthao/workspace/yolov5/my_scripts/aim_esr/annotations/aim_fold_0.yaml \
 # --weights /home/dtpthao/workspace/yolov5/runs/train/yolov5s_fold_$fold/weights/best.pt \
 # --verbose --save-json \
-# --name yolov5s_fold_$fold\_val \
+# --name yolov5s_fold_$fold\_esr_blur_20_val_test \
 
 # Subnet Train
-python /home/dtpthao/workspace/yolov5/my_scripts/train_subnet.py
+# python /home/dtpthao/workspace/yolov5/my_scripts/train_subnet.py
 
 # Subnet Validation
 # fold=0
-# python /home/dtpthao/workspace/yolov5/my_scripts/val_new.py \
+# python /home/dtpthao/workspace/yolov5/my_scripts/val_subnet.py \
 # --img 480 --batch 1 \
-# --data /home/dtpthao/workspace/yolov5/aim_folds/fold_$fold/annotations/aim_fold_$fold.yaml \
+# --data /home/dtpthao/workspace/yolov5/my_scripts/aim_esr/annotations/aim_fold_0.yaml \
 # --weights /home/dtpthao/workspace/yolov5/runs/train/yolov5s_fold_$fold/weights/best.pt \
 # --verbose \
+# --save-json \
 # --project "/home/dtpthao/workspace/yolov5/my_scripts/runs/val" \
-# --name subnet_v41_ce_100epochs_fold_0_fix
+# --name test
+
+fold=0
+python /home/dtpthao/workspace/yolov5/my_scripts/val_subnet.py \
+--img 480 --batch 1 \
+--data /home/dtpthao/workspace/yolov5/aim_folds/fold_0/annotations/aim_fold_0.yaml \
+--weights /home/dtpthao/workspace/yolov5/best.pt \
+--verbose \
+--save-json \
+--project "/home/dtpthao/workspace/yolov5/my_scripts/runs/val" \
+--name test
